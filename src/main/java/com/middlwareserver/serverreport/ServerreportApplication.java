15b5dbea8d2c7369ca3cd9d37ee09c4b393cf562
umtigithub <105437165+umtigithub@users.noreply.github.com>
2025-11-10 16:32:20 -0300

    first commit

---
+++ src/main/java/com/middlwareserver/serverreport/ServerreportApplication.java
@@ -0,0 +1,13 @@
+package com.middlwareserver.serverreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerreportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerreportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/middlwareserver/serverreport/ServerreportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.middlwareserver.serverreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerreportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
